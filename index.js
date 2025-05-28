7c31dda34289284d9db9f70de08ec8638008ac01
abhaypawar47 <abhaypawar0407@gmail.com>
2025-05-28 12:55:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 7000;
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`app working on port: ${port}`);
+});
