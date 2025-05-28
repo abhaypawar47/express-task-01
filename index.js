c06629a80c9c54983a7e52c1f2385491110b7278
abhaypawar47 <abhaypawar0407@gmail.com>
2025-05-28 13:39:28 +0530

    css added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 7000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("home.ejs");
